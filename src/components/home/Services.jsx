ffcc2247e340a040321d4c85686ba7e94f4baafe
mohitkheraliya <kheraliyamohit2004@.com>
2025-07-08 18:50:40 +0530

    add dynamic content for home/service-list section

--- src/components/home/Services.jsx
+++ src/components/home/Services.jsx
@@ -1,6 +1,46 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loading from '../loading/Loading'
 
 const Services = () => {
+  const [serviceContent, setServiceContent] = useState([])
+  const [loading, setLoading] = useState(false)
+  const baseURL = import.meta.env.VITE_BASE_URL
+
+  useEffect(() => {
+    const fetchServicesContent = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch(`${baseURL}/services-list`, {
+          method: 'GET',
+        })
+
+        if (!response.ok) {
+          const errorData = await response.json().catch(() => null)
+          toast.error(errorData?.message, { position: 'top-center' })
+          setLoading(false)
+          return
+        }
+
+        const data = await response.json()
+        setServiceContent(data?.data || [])
+      } catch (error) {
+        toast.error(error || 'There was an issue retrieving services data. Please check your connection.', {
+          className: 'toast-error',
+        })
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchServicesContent()
+  }, [])
+
+  console.log('servicesContent', serviceContent)
+
+  // if (loading) {
+  //   return <Loading />
+  // }
+
   return (
     <section className="space-xl">
       <div className="secondary-container">
@@ -9,75 +49,35 @@ const Services = () => {
             <p className="section-title">Our Services List</p>
             <h2>Services We’re Offering</h2>
           </div>
-          <Link
-            to="/order-now"
-            className="btn"
-            role="button"
-            title="Explore More"
-          >
+          <Link to="/order-now" className="btn" role="button" title="Explore More">
             Explore More
           </Link>
         </div>
 
-        <div className="flex justify-between tab-s:flex-col tab-m:gap-10">
-          <div className="shadow basis-[31%] rounded-3xl py-24 px-20 bg-[#F7F8FD] laptop-l:py-20 laptop-l:px-16 laptop-m:py-16 laptop-m:px-12 laptop:py-14 laptop:px-10 laptop:rounded-2xl laptop-s:py-12 laptop-s:px-8 tab-l:px-6 tab-l:py-10 tab-l:rounded-xl tab-s:p-8 mb:p-6">
-            <div className="flex flex-col items-center gap-16 laptop-l:gap-14 laptop-md:gap-12 laptop-m:gap-10 laptop:gap-10 tab-l:gap-8 tab-s:gap-6">
-              <div className="h-40 laptop-l:h-32 laptop-m:h-28 laptop:h-24 laptop-s:h-20 tab-s:h-24 tab:h-20 mb-l:h-16">
-                <img
-                  src="/services-laundry.png"
-                  alt="Professional laundry service"
-                  className="inline-block laptop-m:max-h-full laptop:h-28 laptop:w-auto tab-m:h-20"
-                />
-              </div>
-              <div className="flex flex-col items-center gap-6 laptop-m:gap-4 laptop-s:gap-3 tab-m:gap-2">
-                <h3>Laundry</h3>
-                <p className="content text-center">
-                  We are professional cleaners in delivering you the highest
-                  quality of services in laundry
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="shadow basis-[31%] rounded-3xl py-24 px-20 bg-[#F7F8FD] laptop-l:py-20 laptop-l:px-16 laptop-m:py-16 laptop-m:px-12 laptop:py-14 laptop:px-10 laptop:rounded-2xl laptop-s:py-12 laptop-s:px-8 tab-l:px-6 tab-l:py-10 tab-l:rounded-xl tab-s:p-8">
-            <div className="flex flex-col items-center gap-16 laptop-l:gap-14 laptop-md:gap-12 laptop-m:gap-10 laptop:gap-10 tab-l:gap-8 tab-s:gap-6">
-              <div className="h-40 laptop-l:h-32 laptop-m:h-28 laptop:h-24 laptop-s:h-20 tab-s:h-24 tab:h-20 mb-l:h-16">
-                <img
-                  src="/services-dry.png"
-                  alt="Professional dry cleaning service"
-                  className="inline-block laptop-m:max-h-full tab-m:h-20"
-                />
-              </div>
-              <div className="flex flex-col items-center gap-6 laptop-m:gap-4 laptop-s:gap-3 tab-m:gap-2">
-                <h3>Dry Cleaning</h3>
-                <p className="content text-center">
-                  We are professional cleaners in delivering the highest quality
-                  dry cleaning services.
-                </p>
+        <div className="flex flex-wrap gap-y-10 gap-x-10 tab-s:flex-col tab-m:gap-10">
+          {serviceContent?.map((service, index) => (
+            <div
+              key={service.service_list_id || index}
+              className="shadow basis-[31%] rounded-3xl py-24 px-20 bg-[#F7F8FD] laptop-l:py-20 laptop-l:px-16 laptop-m:py-16 laptop-m:px-12 laptop:py-14 laptop:px-10 laptop:rounded-2xl laptop-s:py-12 laptop-s:px-8 tab-l:px-6 tab-l:py-10 tab-l:rounded-xl tab-s:p-8 mb:p-6">
+              <div className="flex flex-col items-center gap-16 laptop-l:gap-14 laptop-md:gap-12 laptop-m:gap-10 laptop:gap-10 tab-l:gap-8 tab-s:gap-6">
+                <div className="h-40 laptop-l:h-32 laptop-m:h-28 laptop:h-24 laptop-s:h-20 tab-s:h-24 tab:h-20 mb-l:h-16">
+                  <img
+                    src={service.image}
+                    alt={service.title}
+                    className="inline-block laptop-m:max-h-full laptop:h-28 laptop:w-auto tab-m:h-20 object-contain"
+                  />
+                </div>
+                <div className="flex flex-col items-center gap-6 laptop-m:gap-4 laptop-s:gap-3 tab-m:gap-2">
+                  <h3>{service.title}</h3>
+                  <p className="content text-center">{service.description}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="shadow basis-[31%] rounded-3xl py-24 px-20 bg-[#F7F8FD] laptop-l:py-20 laptop-l:px-16 laptop-m:py-16 laptop-m:px-12 laptop:py-14 laptop:px-10 laptop:rounded-2xl laptop-s:py-12 laptop-s:px-8 tab-l:px-6 tab-l:py-10 tab-l:rounded-xl tab-s:p-8">
-            <div className="flex flex-col items-center gap-16 laptop-l:gap-14 laptop-md:gap-12 laptop-m:gap-10 laptop:gap-10 tab-l:gap-8 tab-s:gap-6">
-              <div className="h-40 laptop-l:h-32 laptop-m:h-28 laptop:h-24 laptop-l:w-32 laptop-s:h-20 laptop-s:w-28 tab-s:h-24 tab:h-20 mb-l:h-16 mb-l:w-24">
-                <img
-                  src="/services-estree.png"
-                  alt="Steam ironing service"
-                  className="inline-block laptop-m:max-h-full tab-m:h-20"
-                />
-              </div>
-              <div className="flex flex-col items-center gap-6 laptop-m:gap-4 laptop-s:gap-3 tab-m:gap-2">
-                <h3>Steam Iron</h3>
-                <p className="content text-center">
-                  We do no more burnt or stained clothes. We deliver clean and
-                  presses clothes.
-                </p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Services;
+export default Services
